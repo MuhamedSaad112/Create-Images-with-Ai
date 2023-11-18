efbbb77f17230912fbf6caaad5715f0837585909
Mohamed Saad <142515671+Mohamed-Saad1@users.noreply.github.com>
2023-11-18 02:11:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
-const api = "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";
+const api = "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";
 const input = document.getElementById('input');
 const images = document.querySelector('.images');
+
 const getImages = async () => {
-  const methods = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${api}`
-    },
-    body: JSON.stringify({
-      "prompt": input.value,
-      "n": 4,
-      "size": "512x512",
-    }),
-  };
-  const res = await fetch(
-    "https://api.openai.com/v1/images/generations",
-    methods
-  );
-  const data = await res.json();
-    const listImages = data.data;
-    images.innerHTML = ''
-    listImages.map(photo => {
-      const container = document.createElement("div");
-      images.append(container);
-      const img = document.createElement("img");
-      container.append(img); 
-      img.src = photo.url;
-    })
+  try {
+    const methods = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${api}`
+      },
+      body: JSON.stringify({
+        "prompt": input.value,
+        "n": 4,
+        "size": "512x512",
+      }),
+    };
+
+    const res = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      methods
+    );
+
+    const data = await res.json();
+
+    if (data && data.data) {
+      const listImages = data.data;
+      images.innerHTML = '';
+      listImages.forEach(photo => {
+        const container = document.createElement("div");
+        images.append(container);
+        const img = document.createElement("img");
+        container.append(img); 
+        img.src = photo.url;
+      });
+    } else {
+      console.error("No data or images found.");
+    }
+  } catch (error) {
+    console.error("Error fetching or processing data:", error);
+  }
 };
